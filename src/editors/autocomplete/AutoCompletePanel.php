1faa0efc8069c250bf270472bf90f58a91a4c76d
MWStudio <yousan4ik@gmail.com>
2018-03-20 15:57:16 +0300

    upd

--- src/editors/autocomplete/AutoCompletePanel.php
+++ src/editors/autocomplete/AutoCompletePanel.php
@@ -1,6 +1,10 @@
 <?php
 namespace editors\autocomplete;
 
+use php\lib\arr;
+use php\lib\str;
+use php\framework\Logger;
+use php\gui\UXApplication;
 use php\gui\event\UXKeyEvent;
 use php\gui\UXListView;
 use php\gui\UXPopupWindow;
@@ -23,18 +27,17 @@ class AutoCompletePanel
      */
     private $listView;
     
+    private $line;
+    
     public function __construct(UXAbstractCodeArea $textArea)
     {
         $this->textArea = $textArea;
+        $this->makeUI();
         $this->init();
     }
     
     private function init()
     {
-        $this->window = new UXPopupWindow();
-        $this->listView = new UXListView();
-        $this->window->add($this->listView);
-        
         $this->textArea->observer('focused')->addListener(function ($old, $new) {
             if (!$new) {
                 $this->hide();
@@ -42,10 +45,11 @@ class AutoCompletePanel
         });
         
         $this->textArea->on('mouseDown', function () {
-            $this->update();
+            $this->hide();
         });
         
         $this->textArea->on('keyDown', function (UXKeyEvent $e) {
+            Logger::info("AutoComplete key : " . $e->codeName);
             $this->update();
             switch ($e->codeName) {
                 case 'Up':
@@ -56,6 +60,7 @@ class AutoCompletePanel
                     break;
                 case 'Enter':
                     $e->consume();
+                    $this->enter();
                     $this->update();
                     break;
                 case 'Left':
@@ -63,13 +68,15 @@ class AutoCompletePanel
                     $this->hide();
                     break;
                 case 'Esc':
+                case 'Backspace':
+                case 'Space':
                     $this->hide();
                     $e->consume();
+                    $this->line = null;
                     break;
                 default:
-                    if ($e->controlDown && $e->codeName == 'Space') {
-                        $e->consume();
-                    }
+                    $this->line .= $e->codeName;
+                    if ($this->line) Logger::info("AutoComplete curent line : " . $this->line);
                     break;
             }
         });
@@ -86,12 +93,45 @@ class AutoCompletePanel
         list($x, $y) = [$caretBounds['x'], $caretBounds['y']];
         $x += $caretBounds['width'];
         $y += $caretBounds['height'];
-        $this->window->show($this->textArea->form, $x, $y);
+        $this->window->show($this->textArea->form, $x + 5, $y);
     }
     
     public function update()
     {
         $this->hide();
+        $arr = $this->listView->items->toArray();
+        $this->listView->items->clear();
+   
+        $this->listView->items->addAll($arr);
         $this->show();
+    }
+    
+    private function makeUI()
+    {
+        $this->window = new UXPopupWindow();
+        $this->listView = new UXListView();
+        $this->listView->maxHeight = 200;
+        $this->listView->maxWidth = 400;
+        $this->listView->focusTraversable = false;
+        
+        $this->window->add($this->listView);
+    }
+    
+    public function add(string $i)
+    {
+        $this->listView->items->add($i);
+    }
+    
+    private function enter()
+    {
+        UXApplication::runLater(function () {
+            $this->hide();
+            
+            $item = $this->listView->selectedItems[0];
+            if ($item == null) return;
+            var_dump($item);
+            
+            uiLater([$this, 'show']);
+        });
     }
 }
